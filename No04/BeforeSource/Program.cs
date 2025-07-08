44540611b393aac1be5ea013c2ba8b0f8fe87d1b
NaoyaTakano <naoya.takano@soft-service.co.jp>
2025-07-08 19:40:32 +0900

    第1回途中経過

--- No04/BeforeSource/Program.cs
+++ No04/BeforeSource/Program.cs
@@ -31,7 +31,7 @@ namespace SampleApp
             try{
                 var list = new List<Item>();
                 var extractItemList = new List<Item>();
-                // エラー1
+                // 読み込み時のエラー(ファイルの存在チェック)
                 var lines = File.ReadAllLines("data.csv");
                 var output = string.Empty;
                 string[] condition = {"A","B","Z"};
@@ -41,16 +41,17 @@ namespace SampleApp
                 {
                     var s = line.Split(',');
                     int quantity;
-                    // エラー2 解決済
+                    // parse時のエラー 解決済
                     if(int.TryParse(s[2], out quantity))
                     {
-                        // CSVデータは要素数足りてる？
+                        // CSVデータは要素数チェック(足りない場合は処理を中断)
                         var item = new Item(s[0], s[1], quantity, s[3], s[4]);
                         list.Add(item);
                     }
                     else
                     {
-                        Console.WriteLine("数量の値が不正");
+                        //　処理スキップするか中断するか続行するか
+                        Console.WriteLine($"{s[0]}の数量が不正");
                     }
                 }
 
@@ -62,13 +63,13 @@ namespace SampleApp
                 foreach(var item in extractItemList){
                     sb.Append(item.Name);
                     sb.Append(string.Join(",", item.Category));
-                    sb.Append(string.Join(",", item.Quantity.ToString()))
+                    sb.AppendLine(string.Join(",", item.Quantity.ToString()));
                 }
 
                 // 出力
                 var dt = DateTime.Now.ToString("yyyyMMddHHmmss");
                 var filename = "output_" + dt + ".csv";
-                // エラー4
+                // 書き込み時のエラー(書き込み権限チェック)
                 File.WriteAllText(filename, output);
 
                 Console.WriteLine("Done.");
