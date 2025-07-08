3e24b0238f2c3cd7126523393674d18143f4a15e
NaoyaTakano <naoya.takano@soft-service.co.jp>
2025-07-08 19:22:58 +0900

    第1回途中経過

--- No04/BeforeSource/Program.cs
+++ No04/BeforeSource/Program.cs
@@ -6,6 +6,24 @@ using System.Linq;
 
 namespace SampleApp
 {
+    class Item
+    {
+        public string Name {get; set;};
+        public string Category {get; set;};
+        public int Quantity {get; set;};
+        public string Location {get; set;};
+        public string Type {get; set;};
+
+        public Item(string name, string category, int quantity, string location, string type)
+        {
+            this.Name = name;
+            this.Category = category;
+            this.Quantity = quantity;
+            this.Location = location;
+            this.Type = type;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -13,16 +31,20 @@ namespace SampleApp
             try{
                 var list = new List<Item>();
                 var extractItemList = new List<Item>();
+                // エラー1
                 var lines = File.ReadAllLines("data.csv");
                 var output = string.Empty;
                 string[] condition = {"A","B","Z"};
 
+                // 行データからアイテムリストを作成
                 foreach (var line in lines)
                 {
                     var s = line.Split(',');
                     int quantity;
+                    // エラー2 解決済
                     if(int.TryParse(s[2], out quantity))
                     {
+                        // CSVデータは要素数足りてる？
                         var item = new Item(s[0], s[1], quantity, s[3], s[4]);
                         list.Add(item);
                     }
@@ -34,41 +56,25 @@ namespace SampleApp
 
                 // LINQで条件抽出
                 extractItemList = list.Where(x => condition.Contains(item.type)).Where(y => !item.Name.Contains("テスト")).Where(z => z.Quantity > 0).ToList();
+                
+                // 文字列結合
                 StringBuilder sb = new StringBuilder();
-
                 foreach(var item in extractItemList){
                     sb.Append(item.Name);
                     sb.Append(string.Join(",", item.Category));
                     sb.Append(string.Join(",", item.Quantity.ToString()))
                 }
 
+                // 出力
                 var dt = DateTime.Now.ToString("yyyyMMddHHmmss");
                 var filename = "output_" + dt + ".csv";
+                // エラー4
                 File.WriteAllText(filename, output);
 
                 Console.WriteLine("Done.");
             }
             catch(Exception ex){
 
-            }
-            
-        }
-
-        class Item
-        {
-            public string Name {get; set;};
-            public string Category {get; set;};
-            public int Quantity {get; set;};
-            public string Location {get; set;};
-            public string Type {get; set;};
-
-            public Item(string name, string category, int quantity, string location, string type)
-            {
-                this.Name = name;
-                this.Category = category;
-                this.Quantity = quantity;
-                this.Location = location;
-                this.Type = type;
             }
         }
     }
